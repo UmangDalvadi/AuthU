f8e0b3d90c7211a3d0f1ce14e60e5d7d5d9d7733
monilprajapati <monillprajapatii@gmail.com>
2024-07-13 23:58:01 +0530

    Dropdown enhancement

--- client/src/components/UserDropdown.jsx
+++ client/src/components/UserDropdown.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { MdKeyboardArrowDown } from "react-icons/md";
@@ -12,7 +12,7 @@ const UserDropdown = ({ mobile }) => {
   const [open, setOpen] = useState(false);
   const { setIsAuth, user } = useUserContext();
   const navigate = useNavigate();
-  console.log("User : ", user);
+  const dropdownRef = useRef(null);
   const links = [
     {
       id: 1,
@@ -20,6 +20,21 @@ const UserDropdown = ({ mobile }) => {
       url: `/profile`,
     },
   ];
+
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const logoutHandler = async () => {
     try {
@@ -41,68 +56,67 @@ const UserDropdown = ({ mobile }) => {
     }
   };
 
+
   return (
     <div>
-      <div className="">
-        <Toaster />
-        <div className="relative">
-          <button
-            id="dropdownDefaultButton"
-            data-dropdown-toggle="dropdown"
-            className={` text-black focus:outline-none font-medium rounded-md text-md px-2 py-1.5 flex items-center ${
-              mobile ? "w-full border border-black bg-white" : "bg-whitesmoke"
-            } justify-between gap-2`}
-            type="button"
-            onClick={() => setOpen(!open)}
+      <Toaster />
+      <div className="relative" ref={dropdownRef}>
+        <button
+          id="dropdownDefaultButton"
+          data-dropdown-toggle="dropdown"
+          className={` text-black focus:outline-none font-medium rounded-md text-md px-2 py-1.5 flex items-center ${
+            mobile ? "w-full border border-black bg-white" : "bg-whitesmoke"
+          } justify-between gap-2`}
+          type="button"
+          onClick={() => setOpen(!open)}
+        >
+          <span className="flex gap-1.5 items-center">
+            <CiUser className="w-6 h-6 rounded-full" />
+            <span className="font-medium">{user?.firstname}</span>
+          </span>
+          <div>
+            {open ? (
+              <MdOutlineKeyboardArrowUp className="text-lg" />
+            ) : (
+              <MdKeyboardArrowDown className="text-lg" />
+            )}
+          </div>
+        </button>
+        {/* Dropdown menu */}
+        {open && (
+          <div
+            id="dropdown"
+            className={`z-100 absolute mt-2  opacity-0 bg-white text-black divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 ${
+              open && `opacity-100`
+            }`}
           >
-            <span className="flex gap-1.5 items-center">
-              <CiUser className="w-6 h-6 rounded-full" />
-              <span className="font-medium">{user.firstname}</span>
-            </span>
-            <div>
-              {open ? (
-                <MdOutlineKeyboardArrowUp className="text-lg" />
-              ) : (
-                <MdKeyboardArrowDown className="text-lg" />
-              )}
-            </div>
-          </button>
-          {/* Dropdown menu */}
-          {open && (
-            <div
-              id="dropdown"
-              className={`z-10 absolute mt-2  opacity-0 bg-white text-black divide-y divide-gray-100 rounded-lg shadow w-full dark:bg-gray-700 ${
-                open && `opacity-100`
-              }`}
-            >
-              <ul className="px-2 font-bold">
-                {links.map((link) => {
-                  const { id, text, url } = link;
-                  return (
-                    <>
-                      <Link
-                        key={id}
-                        to={url}
-                        onClick={() => setOpen(false)}
-                        className="flex px-3 py-2 text-md font-medium border-b border-black border-opacity-40 last:border-none"
-                      >
-                        {text}
-                      </Link>
-                    </>
-                  );
-                })}
-                <button
-                  className="md:flex px-3 py-2 text-md font-medium hidden"
-                  onClick={() => {
-                    logoutHandler();
-                  }}
-                >
-                  Logout
-                </button>
-              </ul>
-            </div>
-          )}
-        </div>
+            <ul className="px-2 font-bold">
+              {links.map((link) => {
+                const { id, text, url } = link;
+                return (
+                  <>
+                    <Link
+                      key={id}
+                      to={url}
+                      onClick={() => setOpen(false)}
+                      className="flex px-3 py-2 text-md font-medium border-b border-black border-opacity-40 last:border-none"
+                    >
+                      {text}
+                    </Link>
+                  </>
+                );
+              })}
+              <button
+                className="md:flex px-3 py-2 text-md font-medium hidden"
+                onClick={() => {
+                  logoutHandler();
+                }}
+              >
+                Logout
+              </button>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
